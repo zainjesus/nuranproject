27962cf80b6101ed95a007f82d82a076f482193e
Zaidm <1337zain1337@mail.ru>
2024-10-19 22:47:09 +0600

    cors

--- projectnuran/settings.py
+++ projectnuran/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'modeltranslation',
     'rest_framework',
     'solo',
+    'corsheaders',
     'drf_yasg',
     'django_cleanup.apps.CleanupConfig',
     'django.contrib.admin',
@@ -32,6 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -40,6 +42,25 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "authorization",
+    "content-type",
+]
+
 
 ROOT_URLCONF = 'projectnuran.urls'
 
